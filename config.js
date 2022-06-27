99af02f1b29cfda3ba730e93522455730b1eb12d
JavierElSheriff <javi.pro.guapo06@gmail.com>
2022-06-27 16:56:40 -0700

    Se agrego mi propio SDK

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 import firebase from 'firebase';
 
 // Agregar SDK Firebase
-var firebaseConfig = {
-
-};
+    const firebaseConfig = {
+        apiKey: "AIzaSyD8XU3dPBB9ZAPDq9_GPpHC_hlXiA7eDME",
+        authDomain: "pro-067.firebaseapp.com",
+        projectId: "pro-067",
+        storageBucket: "pro-067.appspot.com",
+        messagingSenderId: "493327785832",
+        appId: "1:493327785832:web:9e1c674701839e607100dd"
+      };
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
 
