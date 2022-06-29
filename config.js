90e68276f8471d0c8a4891d4888e3a32cb6fb49a
maximiliano280311 <maxelpiloto2@icloud.com>
2022-06-29 15:31:05 -0500

    se agrego mi propio sdk

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 
 // Agregar SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyApskXtHaIQFIP4DYvpowR7DcD4-21CxT4",
+    authDomain: "proyecto-67.firebaseapp.com",
+    databaseURL: "https://proyecto-67-default-rtdb.firebaseio.com",
+    projectId: "proyecto-67",
+    storageBucket: "proyecto-67.appspot.com",
+    messagingSenderId: "976657489867",
+    appId: "1:976657489867:web:639a24af571c0ef78ba04e",
+    measurementId: "G-Y7CTLBW04Z"
 };
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
